fe99f2ef48eb58179560ad9aa8e6158d01e53100
thezohaibkhalid <zohaibkhalid.pk@gmail.com>
2025-04-13 10:24:57 +0500

    for starting a new action

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ limit: '15mb', extended: true }));
     res.send("Hello Zohaib");
 });
 
- app.use("/contact", contactRoute);
+ app.use("/contact", contactRoute);  
+ //for starting the flow again
 
 export default app;
