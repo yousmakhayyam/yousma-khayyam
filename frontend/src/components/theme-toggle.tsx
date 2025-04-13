6989c110b06700861ec4d1f08fac8e4d0f0bab77
thezohaibkhalid <zohaibkhalid.pk@gmail.com>
2025-04-13 22:07:44 +0500

    for pushing another github action CI/CD

--- frontend/src/components/theme-toggle.tsx
+++ frontend/src/components/theme-toggle.tsx
@@ -1,9 +1,9 @@
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "../components/theme-provider"
-import { Button } from "../components/ui/button"
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "../components/theme-provider";
+import { Button } from "../components/ui/button";
 
 export function ThemeToggle() {
-  const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
   return (
     <Button
@@ -15,5 +15,7 @@ export function ThemeToggle() {
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
-  )
+  );
 }
+
+//for update
