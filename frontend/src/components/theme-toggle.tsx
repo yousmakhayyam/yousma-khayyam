12fcf4d43e691b11cc20960a1c0e68fe3fcda935
thezohaibkhalid <zohaibkhalid.pk@gmail.com>
2025-04-13 21:16:04 +0500

    changes in imports to resolve alias issues in the azure deployment testes build in local it's workin

--- frontend/src/components/theme-toggle.tsx
+++ frontend/src/components/theme-toggle.tsx
@@ -1,6 +1,6 @@
 import { Moon, Sun } from "lucide-react"
-import { useTheme } from "@/components/theme-provider"
-import { Button } from "@/components/ui/button"
+import { useTheme } from "../components/theme-provider"
+import { Button } from "../components/ui/button"
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme()
